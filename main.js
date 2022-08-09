6f5d409101320728bb504356f16dec7c73beb230
Karina <kbrilhante@gmail.com>
2022-08-08 21:49:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const ctx = canvas.getContext("2d");
 const roverWidth = 100;
 const roverHeight = 90;
 
+const borderRight = canvas.width - roverWidth;
+const borderBottom = canvas.height - roverHeight;
+
 const backgroundImage = "mars.jpg";
 const roverImage = "rover.png";
 
-const roverX = 10;
-const roverY = 10;
+var roverX = 10;
+var roverY = 10;
 
 var backgroundImgTag, roverImgTag;
 
@@ -33,10 +36,36 @@ function uploadRover() {
 window.addEventListener("keydown", keyDown);
 
 function keyDown (e) {
+    const passo = 10;
     const keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed == 38) {
-        up();
-        console.log('cima');
+    console.log(e.key + " -> " + keyPressed);
+    if(keyPressed == 37) {
+        // esquerda
+        if (roverX > 0) {
+            roverMove(-passo, 0);
+        }
+    } else if (keyPressed == 38) {
+        // cima
+        if (roverY > 0) {
+            roverMove(0, -passo);
+        }
+    } else if (keyPressed == 39) {
+        // direita
+        if (roverX <= borderRight) {
+            roverMove(passo, 0);
+        }
+    } else if (keyPressed == 40) {
+        // baixo
+        if (roverY <= borderBottom) {
+            roverMove(0, passo);
+        }
     }
+}
+
+function roverMove (x, y) {
+    roverX += x;
+    roverY += y;
+
+    uploadBackground();
+    uploadRover();
 }
